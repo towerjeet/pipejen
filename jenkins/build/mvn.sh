95af1a891f973b9e8e754c47bc9cc4d994aa9034
towerjeet <71822996+towerjeet@users.noreply.github.com>
2020-09-24 18:53:23 +0530

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/root/jekins/jenkins_home/workspace/devops
+WORKSPACE=/var/lib/jenkins/workspace/devops
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
